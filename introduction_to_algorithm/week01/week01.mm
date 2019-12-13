5ec062653f086ea4af7c81fdce60465e0930330a
bianyixun <luixiao1223@sina.com>
2019-12-13 16:32:29 +0800

    add

---
+++ introduction_to_algorithm/week01/week01.mm
@@ -0,0 +1,159 @@
+<map version="0.9.0">
+
+<node COLOR="#000000">
+<font NAME="SansSerif" SIZE="20"/>
+<richcontent TYPE="NODE">
+<html>
+<head>
+</head>
+<body>
+<p>提纲
+</p>
+</body>
+</html>
+</richcontent>
+<node COLOR="#0033ff" ID="sec-1" POSITION="right" FOLDED="true">
+<font NAME="SansSerif" SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+
+<richcontent TYPE="NODE">
+<html>
+<head>
+</head>
+<body>
+<p>课程介绍
+</p>
+</body>
+</html>
+</richcontent>
+<richcontent TYPE="NOTE">
+<html>
+<head>
+</head>
+<body>
+<ol class="org-ol">
+<li>MIT 网站资源介绍。</li>
+<li>MIT 6.006 and 6.046,覆盖算法导论</li>
+<li>6.006概览</li>
+</ol>
+</body>
+</html>
+</richcontent>
+</node>
+
+
+<node COLOR="#0033ff" ID="sec-2" POSITION="left" FOLDED="true">
+<font NAME="SansSerif" SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+
+<richcontent TYPE="NODE">
+<html>
+<head>
+</head>
+<body>
+<p>Peak finding
+</p>
+</body>
+</html>
+</richcontent>
+<node COLOR="#00b439" ID="sec-2-1" POSITION="left" FOLDED="false">
+<font NAME="SansSerif" SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+
+<richcontent TYPE="NODE">
+<html>
+<head>
+</head>
+<body>
+<p>1-D
+</p>
+</body>
+</html>
+</richcontent>
+<richcontent TYPE="NOTE">
+<html>
+<head>
+</head>
+<body>
+<ol class="org-ol">
+<li>描述问题</li>
+<li>证明一定有解</li>
+<li>去掉等号的话不一定有解</li>
+<li>straightforward 方法 &amp; 分析复杂度（最坏情况）</li>
+<li>更快的算法(分治法)</li>
+<li>介绍peak finding的$Θ(log<sub>2</sub>(n))$算法</li>
+<li>分析时间复杂度（最坏情况）</li>
+</ol>
+</body>
+</html>
+</richcontent>
+</node>
+
+
+<node COLOR="#00b439" ID="sec-2-2" POSITION="left" FOLDED="false">
+<font NAME="SansSerif" SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+
+<richcontent TYPE="NODE">
+<html>
+<head>
+</head>
+<body>
+<p>2-D
+</p>
+</body>
+</html>
+</richcontent>
+<richcontent TYPE="NOTE">
+<html>
+<head>
+</head>
+<body>
+<ol class="org-ol">
+<li>描述问题</li>
+<li>解一定存在证明</li>
+<li>starightforward方法 &amp; 分析复杂度 （最坏情况）</li>
+<li>贪心算法（思路：根据存在性证明） &amp; 分析复杂度 （最坏情况）</li>
+<li>更快的算法介绍</li>
+<li>如何证明这个算法有效。（利用贪心算法）</li>
+</ol>
+<p>
+7.分析时间复杂度。
+</p>
+</body>
+</html>
+</richcontent>
+</node>
+
+</node>
+
+
+<node COLOR="#0033ff" ID="sec-3" POSITION="right" FOLDED="true">
+<font NAME="SansSerif" SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+
+<richcontent TYPE="NODE">
+<html>
+<head>
+</head>
+<body>
+<p>渐进符号
+</p>
+</body>
+</html>
+</richcontent>
+<richcontent TYPE="NOTE">
+<html>
+<head>
+</head>
+<body>
+<p>
+5个渐进符号
+</p>
+</body>
+</html>
+</richcontent>
+</node>
+
+</node>
+</map>
